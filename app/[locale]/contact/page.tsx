543913340a752f2b71c57c3ab325dc62feeeff0a
espoparis <web@espoparis.com>
2026-09-13 15:30:16 +0300

    Upload RC3 updates

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -54,7 +54,7 @@ export default async function ContactPage(
   };
 
   return (
-    <div className="-mt-24 flex flex-1 flex-col md:-mt-28">
+    <div className="flex flex-1 flex-col">
       <JsonLd
         data={createWebPageJsonLd({
           locale: params.locale,
